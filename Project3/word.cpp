5b2b1378feb1409a0aa41a8900077c9c221f1986
jungwang <jklkwang0727@gmail.com>
2023-07-06 11:22:11 +0900

    [feature] add length compare

--- Project3/word.cpp
+++ Project3/word.cpp
@@ -1,4 +1,22 @@
+#include <string>
+using namespace std;
+
 class wordChecker
 {
-	
+public:
+	int lengthCompare(const string &str1, const string &str2)
+	{
+		int gab = 0;
+		int short_len = 0;
+		if (str1.length() > str2.length()) {
+			gab = str1.length() - str2.length();
+			short_len = str2.length();
+			}
+		else {
+			gab = str2.length() - str1.length();
+			short_len = str1.length();
+		}
+		int score = roundf((1 - ((float)gab / short_len)) * 60);
+		return score;
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,11 +1,16 @@
 #include "pch.h"
+#include "../Project3/word.cpp"
 
 class wordFixture : public testing::Test
 {
 	
 };
 
-TEST(wordFixture, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(wordFixture, lengthCompare) {
+	wordChecker checker;
+
+	EXPECT_EQ(checker.lengthCompare("ASD", "DSA"), 60);
+	EXPECT_EQ(checker.lengthCompare("A", "BB"), 0);
+	EXPECT_EQ(checker.lengthCompare("AAABB", "BAA"), 20);
+	EXPECT_EQ(checker.lengthCompare("AA", "AAE"), 30);
 }
