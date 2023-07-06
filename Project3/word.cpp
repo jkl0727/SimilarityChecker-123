353e515f703c7602a82387b66d44914a1e7a9d1b
jungwang <jklkwang0727@gmail.com>
2023-07-06 11:45:15 +0900

    [feature] alphabet compare add

--- Project3/word.cpp
+++ Project3/word.cpp
@@ -1,4 +1,35 @@
+#include <string>
+using namespace std;
+
 class wordChecker
 {
+public:
+	explicit wordChecker() : alpha_hash{} {}
 	
+	int wordCompare(const string& str1, const string& str2)
+	{
+		memset(alpha_hash, 0, sizeof(alpha_hash));
+
+		int total_count = 0;
+		for (auto ch : str1)
+		{
+			if (alpha_hash[ch - 'A'] == 0) {
+				alpha_hash[ch - 'A'] = 1;
+				total_count++;
+			}
+		}
+
+		int same_count = 0;
+		for (auto ch : str2)
+		{
+			if (alpha_hash[ch - 'A'] == 1) {
+				same_count++;
+				alpha_hash[ch - 'A']++;
+			}
+			else if (alpha_hash[ch - 'A'] == 0) total_count++;
+		}
+		return ((same_count / (float)total_count) * 40);
+	}
+private:
+	int alpha_hash[50];
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,11 +1,15 @@
 #include "pch.h"
+#include "../Project3/word.cpp"
 
 class wordFixture : public testing::Test
 {
 	
 };
 
-TEST(wordFixture, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST_F(wordFixture, CompareAlpha) {
+	wordChecker checker;
+	EXPECT_EQ(checker.wordCompare("ASD", "DSA"), 40);
+	EXPECT_EQ(checker.wordCompare("A", "B"), 0);
+	EXPECT_EQ(checker.wordCompare("AAABB", "BA"), 40);
+	EXPECT_EQ(checker.wordCompare("AA", "AAE"), 20);
 }
